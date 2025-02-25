a4f1111a4f73207c9986cc2c4af024dc553a052e
MQuangTrong <maiquangtrongc2vng@gmail.com>
2025-02-25 18:38:25 +0700

    update route

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -14,21 +14,21 @@ module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin
 
     app.use(
-        PATH_ADMIN + '/dashboard', 
+        PATH_ADMIN,
         authMiddleware.requireAuth,
         dashboardRoutes
     );
-      
-    app.use(PATH_ADMIN + '/products',authMiddleware.requireAuth, productRoutes); 
 
-    app.use(PATH_ADMIN + '/products-category',authMiddleware.requireAuth, productCategoryRoutes); 
-    
-    app.use(PATH_ADMIN + '/roles',authMiddleware.requireAuth, roleRoutes); 
+    app.use(PATH_ADMIN + '/products', authMiddleware.requireAuth, productRoutes);
 
-    app.use(PATH_ADMIN + '/accounts',authMiddleware.requireAuth, accountRoutes); 
+    app.use(PATH_ADMIN + '/products-category', authMiddleware.requireAuth, productCategoryRoutes);
 
-    app.use(PATH_ADMIN + '/auth', authRoutes); 
+    app.use(PATH_ADMIN + '/roles', authMiddleware.requireAuth, roleRoutes);
 
-    app.use(PATH_ADMIN + '/my-account', authMiddleware.requireAuth, myAccountRoutes); 
+    app.use(PATH_ADMIN + '/accounts', authMiddleware.requireAuth, accountRoutes);
+
+    app.use(PATH_ADMIN + '/auth', authRoutes);
+
+    app.use(PATH_ADMIN + '/my-account', authMiddleware.requireAuth, myAccountRoutes);
 }
 
